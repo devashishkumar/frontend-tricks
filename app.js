fca9f9e51b6308c923bbbdb26df970b068f3e98b
Ashish <dev.ashishkumar0512@gmail.com>
2025-01-18 14:41:40 +0530

    remove etag, cacheControl and lastModified headers

--- app.js
+++ app.js
@@ -39,7 +39,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public'), {
+  etag: true,
+  cacheControl: false,
+  lastModified: true
+}));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
