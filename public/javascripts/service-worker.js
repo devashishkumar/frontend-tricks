1ea887d5b5fde664984b8ade5c3511db4a937137
Ashish <dev.ashishkumar0512@gmail.com>
2025-01-19 11:51:39 +0530

    update service worker life cycle

--- public/javascripts/service-worker.js
+++ public/javascripts/service-worker.js
@@ -7,6 +7,21 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", (e) => {
+  // Clean up useless cache
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key != CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
